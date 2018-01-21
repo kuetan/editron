025e1705f08c0dca41370c049ecf76d808a8ba6a
Ykuetan <kue.gamer@gmail.com>
2018-01-21 16:26:21 +0900

    try and error textarea css

--- src/main/createMainWindow.js
+++ src/main/createMainWindow.js
@@ -2,7 +2,7 @@ import {BrowserWindow} from "electron";
 
 class MainWindow {
     constructor(){
-	this.window = new BrowserWindow({ width:800,height:700});
+	this.window = new BrowserWindow({ width:800,height:600});
 	this.window.loadURL('file://' + __dirname + '/../../index.html');
 	this.window.on("closed",()=>{
 	    this.window = null;
